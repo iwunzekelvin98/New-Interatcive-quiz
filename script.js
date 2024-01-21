2f8550a2e99643d105670a8e6f9ddcf0ff0b2213
Chisom Iwunze <chisomiwunze@csus.edu>
2024-01-21 09:06:15 -0800

    Implementing The mouse Options

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", ()=> {
     const quizContainer = document.getElementById('quiz-container');
     const optionElements = document.querySelectorAll('.quiz-options');
 
     optionElements.forEach(option => {
         option.addEventListener('click', () => {
-
             console.log(option.textContent);
         });
+        option.addEventListener('mouseover', () => {
+            option.style.backgroundColor = '#FCBB65';
+        });
+        option.addEventListener('mouseout', () => {
+            option.style.backgroundColor = '';
+        });
+        option.addEventListener('click', (Skin) => {
+            option.style.backgroundColor = '#00FF00'
+        })
 
     }); 
 
