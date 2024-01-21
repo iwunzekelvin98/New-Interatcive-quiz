a29683f0115a15cdb6e383f1a7d304d9b68ea005
Chisom Iwunze <chisomiwunze@csus.edu>
2024-01-21 10:11:01 -0800

    Finished Practice Quiz Project

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
     const quizContainer = document.getElementById('quiz-container');
     const optionElements = document.querySelectorAll('.quiz-options');
 
     optionElements.forEach(option => {
         option.addEventListener('click', () => {
-            console.log(option.textContent);
+            checkAnswer(option.textContent);
         });
         option.addEventListener('mouseover', () => {
             option.style.backgroundColor = '#FCBB65';
@@ -12,12 +12,20 @@ document.addEventListener("DOMContentLoaded", ()=> {
         option.addEventListener('mouseout', () => {
             option.style.backgroundColor = '';
         });
-        option.addEventListener('click', (Skin) => {
-            option.style.backgroundColor = '#00FF00'
-        })
+    });
 
-    }); 
-
+    function checkAnswer(selectedOption) {
+        const correctAnswer = 'C) Skin';
+       optionElements.forEach(option => {
+            if (option.textContent.trim() === selectedOption) {
+                if (selectedOption === correctAnswer) {
+                    option.style.backgroundColor = '#00FF00'; // Green for correct answer
+                } else {
+                    option.style.backgroundColor = '#FF0000'; // Red for incorrect answer 
+                }
+             };
+        });
+    }; 
 });
 
 
